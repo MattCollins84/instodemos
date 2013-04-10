6c8885d45f7d159837889a5999faf45242bf7f42
Matt Collins <matt@strikesandgutters.com>
2013-04-10 20:30:34 +0100

    video demo

--- includes/header.php
+++ includes/header.php
@@ -18,12 +18,7 @@ require_once('includes/config.php');
     <link href="/css/prettify.css" rel="stylesheet">
     <link href="/css/jqueryui.css" rel="stylesheet">
     
-    <!-- share this -->
-    <script type="text/javascript">var switchTo5x=true;</script>
-		<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
-		<script type="text/javascript" src="http://s.sharethis.com/loader.js"></script>
-
-    <script src="/js/jquery.js"></script>
+   	<script src="/js/jquery.js"></script>
     <script src="/js/jqueryui.js"></script>
     <script src="/js/prettify.js"></script>
     <script src="/js/custom.js"></script>
